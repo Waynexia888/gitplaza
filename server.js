508ce9235998f6c99a33212c22d8279b17d5d2bf
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-22 22:59:58 -0700

    updated

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,6 +8,23 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
     const server = new Koa()
+    // const router = new Router()
+
+    // router.get('/test/:id', (ctx) => {
+    //     ctx.body = `<p>request /test ${ctx.params.id}</p>`
+    //     ctx.body = { success: true }
+    //     ctx.set('Content-Type', 'application/json')
+    // })
+
+    // server.use(async (ctx, next) => {
+    //     const path = ctx.path
+    //     const method = ctx.method
+    //     ctx.body = `<span>Koa Render ${method} ${path}</span>`
+    //     await next()
+    // })
+
+    // server.use(router.routes())
+
     server.use(async(ctx, next) => {
         await handle(ctx.req, ctx.res)
         ctx.respond = false
