7a722ed31f7cc18d361682a4b6571398d8bc2e22
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-24 00:50:06 -0700

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+import Router from 'next/router'
 import { Button } from 'antd'
 
-export default () => <Button>Index</Button>
+export default () => {
+    function gotoTestB() {
+        Router.push('/test/b')
+    }
+
+    return (
+        <div>
+            <Link href='/a'>
+                <Button>Index</Button>
+            </Link>
+        <Button onClick={gotoTestB}>test</Button>
+        </div> 
+    )
+}
