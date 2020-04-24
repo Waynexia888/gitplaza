0711941671fd596f2b7b185fef4fd4f4e4235d3a
Waynexia888 <qiwuwaynexia@gmail.com>
2020-04-24 02:02:48 -0700

    updated

--- pages/a.js
+++ pages/a.js
@@ -1,6 +1,9 @@
 import { withRouter } from 'next/router'
+import Link from 'next/link'
 import Comp from '../components/comp'
 
+
+// const A = ({ router }) => <Link href="#aaa"><a>A {router.query.id}</a></Link>
 const A = ({ router }) => <Comp>A {router.query.id}</Comp>
 
 export default withRouter(A)
